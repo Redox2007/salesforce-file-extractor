ce50115e3a17cd4861c7ce3a9d79d1a8cf8b1676
Mani Sundaresan <mani.hologic@gmail.com>
2025-06-05 08:55:38 -0500

    Update App.js with new Connected App configuration

--- src/App.js
+++ src/App.js
@@ -191,7 +191,7 @@ LIMIT 50`);
     const authUrl =
       `${baseUrl}/services/oauth2/authorize?` +
       `response_type=token&` +
-      `client_id=3MVG9dAEux2v1sLvQN9CIido5s9LgvH8XiuwsV0HguqrzZru.s5nuLMBp4H1RdPYJULFC0EHGCg42nE9J0OW3` +
+      `client_id=3MVG9dAEux2v1sLvQN9CIido5s2_uQCIGhhK8n9DyL.yAOQ5vWN5qsK5UXs3E8USdnzDnLWeuA6C.HygME5UZ` +
       `redirect_uri=${encodeURIComponent(redirectUri)}&` +
       `scope=api%20refresh_token&` +
       `state=oauth&` +
